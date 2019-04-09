c66bb0fda5e1513da9d08ba2957bb52c0872c9fa
18SW103 <49424395+18SW103@users.noreply.github.com>
2019-04-09 18:55:42 +0500

    Add files via upload

---
+++ folder/TaskNo1.java
@@ -0,0 +1,17 @@
+class TaskNo1{
+public static void main(String args[]){
+
+String name;
+name="Asra Jatoi";
+System.out.println("Name: "+name);
+short totalmarks=1200;
+System.out.println("Total marks:  "+totalmarks);
+int obtainmarks=999;
+System.out.println("Obtainedmarks:  "+obtainedmarks);
+char grade="A";
+System.out.println("Grade:  "+grade);
+double per=(obtainedmarks*100)/totalmarks;
+System.out.println("percentage:  "+per);
+
+}
+}
---
+++ folder/TaskNo2.java
@@ -0,0 +1,14 @@
+class TaskNo2{
+public static void main(String args[]){
+
+double a=50.25;
+int b=(int)a;
+double c=a-b;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+}
+}
+
+
