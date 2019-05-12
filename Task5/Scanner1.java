def02b40375f2083a50c1efb5f823401517cbbf7
18SW103 <49424395+18SW103@users.noreply.github.com>
2019-05-12 21:45:13 +0500

    Add files via upload

---
+++ Task5/Scanner1.java
@@ -0,0 +1,15 @@
+import java.until.Scanner;
+class Scanner1{
+
+public static void main(String args[]){
+int x=1;
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter no:");
+int n = scan.nextInt();
+for(int i=1;i<=n;i++){
+
+  x +=i;
+  }
+  System.out.println("sum of n number is:"+x);
+  }
+  }
